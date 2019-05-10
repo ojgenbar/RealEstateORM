437febd2660302f6ed4d104a5c2ec57be50fce61
Evgeniy Baryshkov <e.baryshkov@gmail.com>
2019-05-10 16:29:38 +0300

    "get_password_from_pgpass_win", default config

--- connection.py
+++ connection.py
@@ -8,10 +8,24 @@ from sqlalchemy.orm import sessionmaker
 DIRNAME = os.path.dirname(__file__)
 
 
-def _load_config():
+def _create_config_file():
+    import shutil
+
     path = os.path.join(DIRNAME, 'connection_config.json')
-    with open(path) as f:
-        d = json.load(f)
+    src_path = os.path.join(DIRNAME, 'connection_config_default.json')
+
+    shutil.copy2(src_path, path)
+
+
+def _load_config():
+    try:
+        path = os.path.join(DIRNAME, 'connection_config.json')
+        with open(path) as f:
+            d = json.load(f)
+    except FileNotFoundError:
+        _create_config_file()
+        return _load_config()
+
     host = d['host']
     port = d['port']
     database = d['database']
@@ -40,3 +54,31 @@ def create_local_session(*, user=getpass.getuser(), echo=False):
     Session = sessionmaker(bind=engine)
     session = Session()
     return session
+
+
+def get_password_from_pgpass_win(user=getpass.getuser()):
+    path = os.path.expandvars(r'%APPDATA%\postgresql\pgpass.conf')
+    if not os.path.isfile(path):
+        raise FileNotFoundError('File "{}" does not exists!'.format(path))
+
+    with open(path) as f:
+        data = f.read().strip().split('\n')
+
+    data = (row.strip() for row in data if row != '' and not row.startswith('#'))
+
+    target_user = user
+    for row in data:
+        try:
+            items = row.split(':')
+            host, port, db, user, passw = items
+            match = (
+                    (host == HOST or host == '*') and
+                    (db == DATABASE or db == '*') and
+                    (port == '*' or int(port) == PORT) and
+                    (user == target_user or user == '*')
+            )
+            if match:
+                return passw
+        except Exception:
+            pass
+    raise RuntimeError('There is no configuration in "pgpass.conf" which match parameters!')
